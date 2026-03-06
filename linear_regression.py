0662f4a3a7fad24782f6f2c066bf243277e756b9
Gautamv2007 <gautam.rrv@gmail.com>
2026-03-06 13:08:32 +0530

    Update linear regression model and remove raw data

---
+++ linear_regression.py
@@ -0,0 +1,58 @@
+import matplotlib
+matplotlib.use('Qt5Agg') #This extra line of code is for ubuntu users. Ignore it...
+
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+data = pd.read_csv('linear_regression_dataset.csv')
+
+def loss_function(m, b, points):
+    total_error = 0
+    for i in range(len(points)):
+        x = points.iloc[i].YearsExperience
+        y = points.iloc[i].Salary
+
+        total_error += (y - (m*x + b)) ** 2
+    total_error / float(len(points))
+
+    return total_error
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].YearsExperience
+        y = points.iloc[i].Salary
+
+        m_gradient += (-2/n) * (x * (y - (m_now * x + b_now)))
+        b_gradient += (-2/n) * ((y - (m_now * x + b_now)))
+
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+
+    return m, b 
+
+m = 0
+b = 0
+
+L = 0.001
+
+epochs = 1000
+
+for i in range(epochs):
+    if i % 100 == 0:
+        val = loss_function(m, b, data)
+        print(f"Epoch: {i}, Loss: {val}")
+    m, b = gradient_descent(m, b, data, L)
+
+print(m, b)
+
+plt.scatter(data.YearsExperience, data.Salary, color='black')
+plt.plot(list(range(0, 15)), [m*x + b for x in range(0, 15)], color='red')
+plt.show()
+
+
+        
